aaecee429232871135bf0d3f0333cc50b4db61c0
Aftab Baig <aftabaig@gmail.com>
2021-02-16 12:57:36 +0500

    .

--- Sources/UnfionicAuthenticateSDK/UnfionicAuthenticateSDK.swift
+++ Sources/UnfionicAuthenticateSDK/UnfionicAuthenticateSDK.swift
@@ -1,6 +1,9 @@
 import Alamofire
 
 public struct UnfionicAuthenticateSDK {
+    public init() {
+        
+    }
     public func initialize() {
         let request = AF.request("https://swapi.dev/api/films")
         request.responseJSON { (data) in
